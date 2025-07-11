9fc1e0730595242a4ee029a72cd32c0d31ae0c61
Datadrummer-jr <drummerpsp@gmail.com>
2025-07-11 10:46:42 -0400

    Add files via upload

--- Camino_a_Cooperstown.py
+++ Camino_a_Cooperstown.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import json 
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
