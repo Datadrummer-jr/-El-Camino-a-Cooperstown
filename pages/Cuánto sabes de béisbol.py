b897d71cc08cc0241131b8fe313311f7a093e4ec
Datadrummer-jr <drummerpsp@gmail.com>
2025-07-16 20:00:03 -0400

    Add files via upload

--- pages/Cuánto sabes de béisbol.py
+++ pages/Cuánto sabes de béisbol.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Camino_a_Cooperstown as app
 import streamlit as st
 import numpy as np
@@ -16,4 +17,24 @@ def main():
     st.title(title)
 
 if __name__ == "__main__":
+=======
+import Camino_a_Cooperstown as app
+import streamlit as st
+import numpy as np
+from PIL import Image
+
+logo = Image.open("logo.jpg")
+
+title="¿Cuánto sabes de béisbol?"
+
+st.set_page_config(
+    page_title=title,
+    page_icon=logo,
+    layout="wide")
+
+def main():
+    st.title(title)
+
+if __name__ == "__main__":
+>>>>>>> 5b69f133b66fd4280b235a97a2054f5d118aa733
     main()
